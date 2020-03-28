0b3d6af4dda8cd037016ac58bae4ce0fecafcf40
imM47Z <38143854+imM47Z@users.noreply.github.com>
2020-03-28 17:20:25 +0000

    Update script.js

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Animesonline.cc Ads Disabler
-// @version      0.1
+// @version      1.0
 // @author       M47Z
 // @match        *://animesonline.cc/*
 // @grant        none
@@ -8,10 +8,12 @@
 
 (function() {
     'use strict';
+    var adsElementsId = null;
 
     var IsPageLoad = ( ) => {
-        return document.getElementById( "contenedor" ) != null;
+        return document.getElementById( "dt_contenedor" ) != null;
     }
+
     var WaitForPage = ( callbackFunction ) => {
         if ( !IsPageLoad( ) )
             setTimeout( () => WaitForPage( callbackFunction ), 1000 );
@@ -19,19 +21,50 @@
         callbackFunction( );
     }
 
-    WaitForPage( ( ) => {
+    var GetAdsBannersId = ( ) => {
         var adsElement = document.getElementsByClassName( "abc" )[0].nextElementSibling;
-        adsElement.parentElement.removeChild( adsElement );
+        if ( adsElement == null )
+            return null;
 
+        if( !adsElement.id.includes( "_yrw" ) )
+            return null;
+
+        return adsElement.id;
+    }
+
+    var RemoveAdsBanners = ( ) => {
+        if ( adsElementsId == null )
+            adsElementsId = GetAdsBannersId( );
+
+        for( ; document.getElementById( adsElementsId ) != null; )
+            document.getElementById( adsElementsId ).remove( );
+
+        return;
+    }
+
+    var RemoveAdsScripts = ( ) => {
         var scriptsArray = document.getElementsByTagName( "script" );
         for( var i = 0; i < scriptsArray.length; i++ )
         {
-            if( !scriptsArray[ i ].src.includes( "online.js" ) )
+            if( !scriptsArray[ i ].src.includes( "online.js" ) && !scriptsArray[ i ].src.includes( "propellerclick" ) )
                 continue;
 
-            scriptsArray[ i ].parentElement.removeChild( scriptsArray[ i ] );
+            scriptsArray[ i ].remove( );
         }
+    }
 
-        console.log( "GG Ads Removida!" );
+    var oAddEventListener = window.document.addEventListener;
+    window.document.addEventListener = ( type, listener, useCapture ) => {
+        return true;
+    }
+
+    WaitForPage( ( ) => {
+        var adsBannersInterval = setInterval( RemoveAdsBanners, 500);
+        var adsScriptsInterval = setInterval( RemoveAdsScripts, 1000);
+
+        setTimeout( ( ) => {
+            clearInterval( adsScriptsInterval );
+            clearInterval( adsBannersInterval );
+        }, 1000);
     });
 })();
