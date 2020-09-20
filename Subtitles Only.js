3dabe55227173706ee58ddf3eac3b3d58577a31e
imM47Z <38143854+imM47Z@users.noreply.github.com>
2020-09-20 15:16:44 +0100

    version change

--- Subtitles Only.js
+++ Subtitles Only.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Subtitles Only
-// @version      1.0
+// @version      1.1
 // @author       M47Z
 // @match        *://animesonline.cc/episodio/*
 // @updateURL    https://raw.githubusercontent.com/imM47Z/animesonline.cc-Script-Pack/master/Subtitles%20Only.js
