7ebc76ba2192c12c04016fdb6936005c0ca8b719
M47Z <38143854+IamM47Z@users.noreply.github.com>
2020-12-26 00:28:50 +0000

    Create animeshouse_popup_remover.js

---
+++ animeshouse_popup_remover.js
@@ -0,0 +1,15 @@
+// ==UserScript==
+// @name         Popup Remover
+// @version      0.1
+// @author       M47Z
+// @match        *://animeshouse.net/episodio/*
+// @match        *://animeshouse.biz/v/*
+// @grant        none
+// ==/UserScript==
+
+(function() {
+    'use strict';
+
+    window.open = null;
+
+})();
