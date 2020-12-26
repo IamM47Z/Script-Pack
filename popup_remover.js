30a5bd95ce3eecd2ed3448d30005ea2a7afd8f8d
M47Z <38143854+IamM47Z@users.noreply.github.com>
2020-12-26 14:06:46 +0000

    it doesnt need a blacklist lol

--- popup_remover.js
+++ popup_remover.js
@@ -1,6 +1,6 @@
 // ==UserScript==
 // @name         Popup Remover
-// @version      1.0
+// @version      1.1
 // @author       M47Z
 // @match        *://animeshouse.net/episodio/*
 // @match        *://animeshouse.biz/v/*
@@ -10,27 +10,10 @@
 (function() {
     'use strict';
 
-    if ( !String.prototype.hasElemOfList )
-    {
-        Object.defineProperty( String.prototype, 'hasElemOfList', {
-            get: ( ) => function( ...args )
-            {
-                let value = this.valueOf( );
-
-                for ( let i = 0, l = args.length; i < l; i++ )
-                    if ( value.indexOf( arguments[ i ] ) > -1 )
-                        return true;
-
-                return false;
-            }
-        } );
-    }
-
     let oAppendChild = document.body.appendChild.bind( document.body );
     document.body.appendChild = ( elem ) =>
     {
-        if ( elem.tagName.toLowerCase( ) != "script" ||
-            ( elem.type.toLowerCase( ) != "text/javascript" && !elem.src.toLowerCase( ).hasElemOfList( "adsco", "displayvertising", "advertisers", "awaitcola" ) ) )
+        if ( elem.tagName.toLowerCase( ) != "script" )
             return oAppendChild( elem );
 
         return true;
